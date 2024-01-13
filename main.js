cbc67d84dda03d10deb89d0bcb1306de77c47124
nurjaxon <nurjaxonazimov>
2024-01-13 17:15:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// let son = +prompt("son kiriting")
+
+
+
+// for (let i = 0;son % 2== 0 <= 10; i++ ) {
+//     console.log();
+//     son++
+// }
+
+
+
+// for(let i = 0; i <= 10;i ++){
+//     console.log(i);
+//     son++
+// }
+
+// if (son % 2 == 0){
+//     console.log(son <= 10);
+// }
+// 
+
+// for (let i = 0;i <=10; i++){
+//     console.log(son);
+//         console.log(i <= 10);
+//         son++
+//     }
+
+
+// for (let i = 1; i % 2 == 0; i++){
+//     console.log();
+//     son++                                                                              
+// }
+
+
+
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+arr[99]=100;
+
+console.log(arr);
+
+// console.log(arr.length);
+
+// arr.forEach(function(chopchak, yurti, arr){;
+// console.log(`${index}: ${item} in to arr ${arr}`);
+// });
+
+// for(let value of arr)
+// console.log(value);
+
+// arr[5]
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+// arr. pop();
+// console.log(arr);
+// arr.push(7)
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift(0)
+// console.log(arr);
+
+
+
+
+
+
+
+
+
